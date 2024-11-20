dee4a34b3a76403db93a35a7c41b5427861155e2
tshruthirao <tshruthirao16@gmail.com>
2024-11-20 13:06:00 +0530

    Add files via upload

---
+++ src/main/java/com/maven/project1/my_maven_project/App.java
@@ -0,0 +1,10 @@
+package com.maven.project1.my_maven_project;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
